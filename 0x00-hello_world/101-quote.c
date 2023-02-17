8df5917352f3567558afe9ed55e8644a30c4148f
zainabouhassi <zaisrine@gmail.com>
2023-02-16 17:20:35 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
 #include <unistd.h>
-
-
 /**
- * main - prints exactly and that piece of art is useful
+ * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	retun(1);
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(STDERR_FILENO, msg, sizeof(msg) - 1);
+
+	return (1);
 }
