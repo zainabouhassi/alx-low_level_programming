69053ac3b89cd5163dc006f6e4dc9cf7f9b9aaaa
zainabouhassi <zaisrine@gmail.com>
2023-02-17 17:19:33 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,22 +7,12 @@
  */
 int main(void)
 {
-	int i, j;
+	char ch;
 
-	for (i = '0'; i < '9'; i++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-	for (j = i + 1; j <= '9'; j++)
-	{
-	if (j != i)
-	{
-	putchar(i);
-	putchar(j);
-	if (i == '8' && j == '9')
-	continue;
-	putchar(',');
-	putchar(' ');
-	}
-	}
+	putchar(ch);
+
 	}
 	putchar('\n');
 	return (0);
